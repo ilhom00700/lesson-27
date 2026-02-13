17bc0888c102a8f49fa70240b3eb8968ed48cdb5
ilhom00700 <ilhomvalijonov007@gmail.com>
2026-02-13 14:46:12 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+    // 1
+// let A = 3
+// let B = 2
+// let Y = -(B / 2) * (A ** 2 + B ** 2)
+// console.log(Y)
+    // 2
+// let A = 3, B = 2, C = 4, D = 1
+// let Y = ((A + B) / (C + D)) ** 2 + B ** 2
+// console.log(Y)
+    // 3
+// let A = 2, C = 3, D = 2
+// let Y = 2 * (A ** 2 + (4 * C ** 2) / 3)
+// console.log(Y)
+    // 4
+// let A = 2, B = 5, C = 3, D = 1
+// let Y = (1 / (A ** 2)) * ((B / 10) ** 3) * ((C + D) ** 2)
+// console.log(Y)
+    // 5
+// let A = 6, B = 2, C = 8, D = 4
+// let Y = ((A / B - 1) / (C / D - 1)) ** 2
+// console.log(Y)
